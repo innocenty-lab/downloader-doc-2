54ff1317c832b348981514476353c9c473f1df89
innocenty-lab <putraramadhan532@gmail.com>
2021-11-08 12:33:24 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:path_provider/path_provider.dart' as p;
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:progress_dialog/progress_dialog.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,36 +29,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
   String? _fileFullPath;
   String? progress;
   bool _isLoading = false;
-  final urlPdf = "http://www.pdf995.com/samples/pdf.pdf";
+  // final urlPdf = "http://www.pdf995.com/samples/pdf.pdf";
+  final urlPdf = "https://kg.bisakode.com/api/dokumen/4/download";
 
   Dio? dio;
+
   @override
   void initState() {
+    createFolder('KGTesting');
     dio = Dio();
     super.initState();
   }
 
-  Future <List<Directory>?> _getExternalStoragePath(){
-    return p.getExternalStorageDirectories(type: p.StorageDirectory.documents);
+  //================================Buat Folder Download==============================
+  Future<String> createFolder(String fName) async {
+    final folderName = fName;
+    final path = Directory("storage/emulated/0/$folderName");
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.manageExternalStorage.request();
+      await Permission.storage.request();
+    }
+    if ((await path.exists())) {
+      return path.path;
+    } else {
+      path.create();
+      return path.path;
+    }
   }
 
-  Future _downloadAndSaveFileToStorage(
-      BuildContext context, String urlPath, String fileName) async {
+  //=========================Get App Direktory==========================================
+  // Future <List<Directory>?> _getExternalStoragePath(){
+  //   return p.getExternalStorageDirectories(type: p.StorageDirectory.documents);
+  // }
+
+  //================================ Downloader Document====================================
+  Future _downloadAndSaveFileToStorage(String urlPath) async {
+
+    final name = urlPdf.split('/').last;
+
     ProgressDialog pr;
-    pr = new ProgressDialog(context, type: ProgressDialogType.Normal);
+    pr = ProgressDialog(context, type: ProgressDialogType.Normal);
     pr.style(message: "Download file ...");
 
     try{
+
       //================== show dialog ==================
       await pr.show();
 
-      final dirList = await _getExternalStoragePath();
-      final path = dirList![0].path;
-      final file = File('$path/$fileName');
-      await dio!.download(urlPath, file.path, onReceiveProgress: (rec, total){
+      // final dirList = await _getExternalStoragePath();
+      // final path = dirList![0].path;
+      // final file = File('$path/$name');
+
+      final Directory _documentDir = Directory('/storage/emulated/0/KGTesting/$name');
+
+      // await dio!.download(urlPath, file.path, onReceiveProgress: (rec, total){
+      await dio!.download(urlPath, _documentDir.path, onReceiveProgress: (rec, total){
         setState(() {
           _isLoading = true;
           progress = ((rec / total)*100).toStringAsFixed(0) + " %";
@@ -71,7 +103,8 @@ class _MyHomePageState extends State<MyHomePage> {
       //============== hide dialog ==============
       pr.hide();
 
-      _fileFullPath = file.path;
+      _fileFullPath = _documentDir.path;
+      
     } catch (e) {
       print(e);
     }
@@ -79,6 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _isLoading = false;
     });
+
   }
   
   @override
@@ -93,11 +127,10 @@ class _MyHomePageState extends State<MyHomePage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              RaisedButton(
+              ElevatedButton(
                 child: Text('Download to External Storage'),
                 onPressed: () {
-                  _downloadAndSaveFileToStorage(
-                    context, urlPdf, "document2.pdf");
+                  _downloadAndSaveFileToStorage(urlPdf);
                 },
               ),
               Text('Writen: $_fileFullPath'),
@@ -107,4 +140,5 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
 }
